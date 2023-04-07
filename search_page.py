0ce1b6cdbe7181852e8086f0cea9890527da515e
AllaDezhur <123001126+AllaDezhur@users.noreply.github.com>
2023-04-07 19:05:27 +0300

    Update search_page.py

--- search_page.py
+++ search_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from base_page import BasePage
 
 class YaSearchPageLocators:
-    LOCATOR_YANDEX_NAVI_BAR = (By.ID, "hdtb-msb")
+   LOCATOR_YANDEX_NAVI_BAR = (By.CLASS_NAME, "hdtb-mitem")
 
 class SearchPageHelper(BasePage):
     def check_navi_bar(self):
